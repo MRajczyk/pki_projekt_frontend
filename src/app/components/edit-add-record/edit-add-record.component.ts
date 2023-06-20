eeb919fa5bfd44dc1adca6fa35f3f93e7d5456d5
MRajczyk <mikolajrajczyk01@gmail.com>
2023-06-20 08:42:19 +0200

    implemented adding records

--- src/app/components/edit-add-record/edit-add-record.component.ts
+++ src/app/components/edit-add-record/edit-add-record.component.ts
@@ -41,9 +41,7 @@ export class EditAddRecordComponent {
     }});
   }
 
-  applyChanges() {
-    this.clearResultInfoParagraph();
-    console.log(this.tableInfo)
+  updateRecord() {
     let setString : string = " set "
     let i = 0;
     this.tableInfo?.columns.forEach(column => {
@@ -69,7 +67,6 @@ export class EditAddRecordComponent {
     })
     this.dbService.sendQuery('UPDATE ' + this.passedData + setString + columns).subscribe({
       next: (val) => {
-        console.log(val)
         this.clearResultInfoParagraph();
         this.router.navigate(['/view'], { state: {
             type: 'table_name',
@@ -84,11 +81,54 @@ export class EditAddRecordComponent {
     })
   }
 
-  // editRecord() {
-  //   this.clearResultInfoParagraph();
-  //   this.router.navigate(['/view'], { state: {
-  //       type: 'table_name',
-  //       data: this.passedData
-  //   }});
-  // }
+  addRecord() {
+    let setString : string = "("
+    let i = 0;
+    this.tableInfo?.columns.forEach(column => {
+      let column_input = document.getElementById(`input_${column.column_name}`) as HTMLInputElement
+      if(column_input.value.length > 0) {
+        if(i !== 0) {
+          setString += ", "
+        }
+        setString += column.column_name;
+        ++i;
+      }
+    })
+    setString += ") values ('"
+    i = 0;
+    this.tableInfo?.columns.forEach(column => {
+      let column_input = document.getElementById(`input_${column.column_name}`) as HTMLInputElement
+      if(column_input.value.length > 0) {
+        if(i !== 0) {
+          setString += ", '"
+        }
+        setString += column_input.value + "'";
+        ++i;
+      }
+    })
+    setString += ")"
+    this.dbService.sendQuery('INSERT INTO ' + this.passedData + setString).subscribe({
+      next: (val) => {
+        this.clearResultInfoParagraph();
+        this.router.navigate(['/view'], { state: {
+            type: 'table_name',
+            data: this.passedData
+          }});
+      },
+      error: (e) => {
+        const errorParagraph = document.getElementById("result_message") as HTMLInputElement
+        errorParagraph.innerHTML = 'Could not delete record, error: <b>' + e.error.error_message + '</b>';
+        console.log(e.error.error_message)
+      }
+    })
+  }
+
+  applyChanges() {
+    this.clearResultInfoParagraph();
+    if(this.mode === 'edit') {
+      this.updateRecord()
+    } else if(this.mode === 'insert') {
+      this.addRecord();
+    }
+  }
 }
